7f6473f7b84376a4440b2b4ff2fbe24eca607345
jasonlai0303 <jasonlai91213@gmail.com>
2025-05-25 13:17:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -46,12 +46,12 @@ def calculate_value(pf):
         shares = data["shares"]
         cost = data["cost"]
         if symbol == "CASH":
-        price = 1.0
-    elif shares < 0:
-        price = cost  # 當作賣出價格
-    else:
-        price = fetch_price(symbol)
-        price_cache[symbol] = price
+            price = 1.0
+        elif shares < 0:
+            price = cost  # 當作賣出價格
+        else:
+            price = fetch_price(symbol)
+            price_cache[symbol] = price
         if price is not None:
             value = price * shares
             cost_total = cost * shares
